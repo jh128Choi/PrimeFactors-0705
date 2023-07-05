cfcec592f68877820af1cbd49c3c59c4d5145620
JunhyeokChoi <anchovy1228@naver.com>
2023-07-05 15:11:23 +0900

    feature 2

--- PrimeFactors/test.cpp
+++ PrimeFactors/test.cpp
@@ -9,3 +9,9 @@ TEST(PrimeFactors, Of1) {
 	vector<int> expected = {};
 	EXPECT_EQ(expected, primeFactor.of(1));
 }
+
+TEST(PrimeFactors, Of2) {
+	PrimeFactor primeFactor;
+	vector<int> expected = {2};
+	EXPECT_EQ(expected, primeFactor.of(2));
+}
